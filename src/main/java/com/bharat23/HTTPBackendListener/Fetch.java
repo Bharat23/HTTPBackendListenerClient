bec908421d2baf784621fb5a266daef4a32b26b5
Bharat Sinha <bharat.sinha.2307@gmail.com>
2019-08-12 21:32:08 -0600

    first push

---
+++ src/main/java/com/bharat23/HTTPBackendListener/Fetch.java
@@ -0,0 +1,136 @@
+package com.bharat23.HTTPBackendListener;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.net.HttpURLConnection;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import com.google.gson.Gson;
+import java.util.zip.GZIPOutputStream;
+
+class Fetch {
+
+    private String enableCompression = "false";
+
+    Fetch (String enableCompression) {
+        this.enableCompression = enableCompression;
+    }
+
+    String post(String url, Map<String, Object> data) {
+        try {
+            URL apiURL = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) apiURL.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setDoOutput(true);
+            if (this.enableCompression.equals("true")) {
+                con.setRequestProperty("Content-Encoding", "gzip");
+                GZIPOutputStream wr = new GZIPOutputStream(con.getOutputStream());
+                Gson gsonObj = new Gson();
+                String jsonStr = gsonObj.toJson(data);
+                wr.write(jsonStr.getBytes());
+                wr.flush();
+                wr.close();
+            } else {
+                con.setRequestProperty("Content-Encoding", "utf-8");
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                Gson gsonObj = new Gson();
+                String jsonStr = gsonObj.toJson(data);
+                wr.writeBytes(jsonStr);
+                wr.flush();
+                wr.close();
+            }
+
+            int responseCode = con.getResponseCode();
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            //print result
+            System.out.println(response.toString());
+            con.disconnect();
+            return response.toString();
+        }
+        catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return "{}";
+    }
+
+    String post(String url, ArrayList data) {
+        try {
+            URL apiURL = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) apiURL.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setDoOutput(true);
+            if (this.enableCompression.equals("true")) {
+                con.setRequestProperty("Content-Encoding", "gzip");
+                GZIPOutputStream wr = new GZIPOutputStream(con.getOutputStream());
+                Gson gsonObj = new Gson();
+                String jsonStr = gsonObj.toJson(data);
+                System.out.println("NOW");
+                System.out.println(jsonStr);
+                wr.write(jsonStr.getBytes());
+                wr.flush();
+                wr.close();
+            } else {
+                con.setRequestProperty("Content-Encoding", "utf-8");
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                Gson gsonObj = new Gson();
+                String jsonStr = gsonObj.toJson(data);
+                wr.writeBytes(jsonStr);
+                wr.flush();
+                wr.close();
+            }
+            int responseCode = con.getResponseCode();
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            //print result
+            System.out.println(response.toString());
+            con.disconnect();
+            return response.toString();
+        }
+        catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return "{}";
+    }
+
+    Map get(String url) {
+        try {
+            URL apiURL = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) apiURL.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-Type", "application/json");
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            //print result
+            System.out.println(response.toString());
+            con.disconnect();
+            Gson gsonObj = new Gson();
+            Map map = gsonObj.fromJson(response.toString(), Map.class);
+            return map;
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        return new HashMap<>();
+    }
+}
---
+++ src/main/java/com/bharat23/HTTPBackendListener/HTTPBackendListenerClient.java
@@ -0,0 +1,93 @@
+package com.bharat23.HTTPBackendListener;
+
+import org.apache.jmeter.assertions.AssertionResult;
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.visualizers.backend.AbstractBackendListenerClient;
+import org.apache.jmeter.visualizers.backend.BackendListenerContext;
+
+import java.net.InetAddress;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimerTask;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HTTPBackendListenerClient extends AbstractBackendListenerClient {
+    
+    private long time = System.currentTimeMillis();
+    private ArrayList<Object>dataObjectList = new ArrayList<Object>();
+
+    @Override
+    public Arguments getDefaultParameters() {
+        Arguments arguments = new Arguments();
+        // my arguments
+        arguments.addArgument("protocol", "http");
+        arguments.addArgument("hostName", "127.0.0.1");
+        arguments.addArgument("url", "/senddata");
+        arguments.addArgument("port", "80");
+        arguments.addArgument("interval", "30"); //in seconds
+        arguments.addArgument("compression", "false");
+        return arguments;
+    }
+
+    //create a map of response object
+    private Map<String, Object> getMap(SampleResult result) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("AllThreads", result.getAllThreads());
+        map.put("Assertions", result.getAssertionResults());
+        // map.put("BodySize", result.getBodySize()); //deprecated
+        map.put("BodySize", result.getBodySizeAsLong());
+        // map.put("Bytes", result.getBytes()); //deprecated
+        map.put("Bytes", result.getBytesAsLong());
+        map.put("ConnectTime", result.getConnectTime());
+        map.put("ContentType", result.getContentType());
+        map.put("DataType", result.getDataType());
+        map.put("EndTime", result.getEndTime());
+        map.put("ErrorCount", result.getErrorCount());
+        map.put("IdleTime", result.getIdleTime());
+        map.put("Latency", result.getLatency());
+        map.put("ResponseCode", result.getResponseCode());
+        map.put("ResponseMessage", result.getResponseMessage());
+        map.put("ResponseTime", result.getTime());
+        map.put("SampleCount", result.getSampleCount());
+        map.put("SampleLabel", result.getSampleLabel());
+        map.put("StartTime", result.getStartTime());
+        map.put("Success", result.isSuccessful());
+        map.put("ThreadName", result.getThreadName());
+        map.put("URL", result.getURL());
+        map.put("timestamp", result.getTimeStamp());
+        return map;
+    }
+
+    @Override
+    public void handleSampleResults(List<SampleResult> results, BackendListenerContext context) {
+        for(SampleResult result : results) {
+            Map<String,Object> jsonObject = getMap(result);
+            long interval = Long.parseLong(context.getParameter("interval")) * 1000;
+            long timeAgo = System.currentTimeMillis() - interval;
+            this.dataObjectList.add(jsonObject);
+            if (this.time < timeAgo) {
+                // time passed
+                Fetch fetch = new Fetch(context.getParameter("compression"));
+                fetch.post((context.getParameter("protocol") + "://" + context.getParameter("hostName") + ":" + context.getParameter("port") + context.getParameter("url")), this.dataObjectList);
+                this.time = System.currentTimeMillis();
+                this.dataObjectList.clear();
+            }
+        }
+    }
+
+    @Override
+    public void teardownTest(BackendListenerContext context) throws Exception {
+        if (this.dataObjectList.size() > 0) {
+            Fetch fetch = new Fetch(context.getParameter("compression"));
+            fetch.post((context.getParameter("protocol") + "://" + context.getParameter("hostName") + ":" + context.getParameter("port") + context.getParameter("url")), this.dataObjectList);
+            this.dataObjectList.clear();
+        }
+        super.teardownTest(context);
+    }
+}
